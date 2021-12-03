81cc43b744634fffb3b844092b156dde02dfe449
kcperidot <kcperidot@gmail.com>
2021-12-02 16:15:13 -0800

    Title and snippet error fixed, details page not fixed

--- app/src/main/java/com/example/test/MapsActivity.java
+++ app/src/main/java/com/example/test/MapsActivity.java
@@ -72,14 +72,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (e == null) {
                     for(int i = 0; i < restrooms.size(); i++) {
                         LatLng rrLocation = new LatLng(restrooms.get(i).getParseGeoPoint("Location").getLatitude(), restrooms.get(i).getParseGeoPoint("Location").getLongitude());
-                        googleMap.addMarker(new MarkerOptions().position(rrLocation).title(restrooms.get(i).getString("Name")).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                        Marker marker = googleMap.addMarker(new MarkerOptions().position(rrLocation).title(restrooms.get(i).getString("Name")).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
                         int finalI = i;
+                        marker.setTitle(restrooms.get(finalI).getString("username"));
+                        String name = marker.getTitle();
+                        marker.setSnippet(restrooms.get(finalI).getString("Category"));
+                        String category = marker.getSnippet();
                         googleMap.setOnMarkerClickListener(marker1 -> {
-                            marker1.setTitle(restrooms.get(finalI).getString("username"));
-                            String name = marker1.getTitle();
                             //String status = restrooms.get(finalI).getString("Status");
-                            marker1.setSnippet(restrooms.get(finalI).getString("Category"));
-                            String category = marker1.getSnippet();
                             //int rating = restrooms.get(finalI).getNumber("Rating").intValue();
                             Intent i1 = new Intent(MapsActivity.this, DetailsActivity.class);
                             i1.putExtra("name", name);
