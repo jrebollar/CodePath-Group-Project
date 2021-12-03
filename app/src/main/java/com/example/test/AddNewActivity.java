f223344d16c71b1d7da062e18f2a596cc6528b5e
Justin Rebollar <jrebollar@cpp.edu>
2021-12-02 21:18:26 -0800

    Implemented FloatingActionButton for add and filter activities

--- app/src/main/java/com/example/test/AddNewActivity.java
+++ app/src/main/java/com/example/test/AddNewActivity.java
@@ -3,12 +3,23 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddNewActivity extends AppCompatActivity {
+
+    public static final String TAG = "AddNewActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+
+
+
     }
 }
